0d45086e443a7cb2df3b97039f6e19950ad61da7
amo-nyarko esther <pcspida@ubuntu.(none)>
2012-07-13 21:40:39 +0000

    part5blog

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('',
     url(r'^posts/$', 'blog.views.post_list'),
     url(r'^posts/(?P<ID>\d+)/((?P<showComments>.*)/)?$', 'blog.views.post_detail'),
     ## add your url here
-    url(r'^posts/search/(?P<anything>.+)/$', 'blog.views.post_search')
+    url(r'^posts/search/(?P<anything>.+)/$', 'blog.views.post_search'),
+    url(r'^comments/(?P<comm_id>\d+)/edit','blog.views.edit_comment'),
 )
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@
 from django.template import Context, loader, Template
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
+from django.forms import ModelForm
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 from models import Post, Comment 
 
 
@@ -18,11 +20,22 @@ def post_list(request):
         each_post += str(item) + ' '"""
     return HttpResponse(t.render(c))
 
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['post_comment']
+@csrf_exempt
 def post_detail(request, ID, showComments=False):
-    
     posts = Post.objects.get(id = ID)
-    
     comments = posts.comment_set.all()
+    if request.method == 'POST':
+        comment = Comment(post = posts)
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(request.path)
+    else:
+        form = CommentForm()
     #t = loader.get_template('blog/post_detail.html')
     #C = Context({'post_detail': posts, 'comment':comments})
     """if showComments != None:
@@ -32,10 +45,20 @@ def post_detail(request, ID, showComments=False):
     Html = "<html><body> post_title: %s</br> comments: %s </body></html> "% (posts , comments)
     #output = str(posts) + ' ' + comments"""
     #return HttpResponse(t.render(C))
-    return render_to_response('blog/post_detail.html', {'post_detail': posts, 'comment':comments})
-    
-    
-    
+    return render_to_response('blog/post_detail.html', {'post_detail': posts, 'comment':comments, 'form':form})
+@csrf_exempt
+def edit_comment(request, comm_id):
+    comments = Comment.objects.get(id = comm_id)
+    posts = comments.post_comment
+    if request.method == 'POST':
+        
+        form = CommentForm(request.POST, instance = comments)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(posts.get_absolute_url())
+    else:
+        form = CommentForm(instance = comments)
+        return render_to_response('blog/edit_comment.html', { 'edit_comment': comments, 'comm_post':posts, 'form':form})
 def post_search(request, anything):
     result = Post.objects.filter(body__icontains = anything)
     #title_body = ''
@@ -49,6 +72,7 @@ def post_search(request, anything):
     
     #return HttpResponse( t.render(c))
     return render_to_response('blog/post_search.html', {'results':result})
+
 def home(request):
     """print 'it works'
     return HttpResponse('hello world. Ete zene?') """
